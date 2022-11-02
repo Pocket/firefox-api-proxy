e05bf76888d4d93523634ebefbf01ead8a7ba3e5
Spencer Balogh <sbalogh@mozilla.com>
2022-11-02 11:39:12 -0500

    rough swag at deployment, likely needs more attention still
    - attempted to remove redis cache
    - point to correct health check
    - fill in missing service name

--- .aws/src/config/index.ts
+++ .aws/src/config/index.ts
@@ -34,7 +34,7 @@ export const config = {
   healthCheck: {
     command: [
       'CMD-SHELL',
-      'curl -f http://localhost:4001/.well-known/apollo/server-health || exit 1',
+      'curl -f http://localhost:4242/.well-known/server-health || exit 1',
     ],
     interval: 15,
     retries: 3,
--- .aws/src/main.ts
+++ .aws/src/main.ts
@@ -8,11 +8,9 @@ import {
 import { AwsProvider, kms, datasources, sns } from '@cdktf/provider-aws';
 import { config } from './config';
 import {
-  ApplicationRedis,
   PocketALBApplication,
   PocketECSCodePipeline,
   PocketPagerDuty,
-  PocketVPC,
 } from '@pocket-tools/terraform-modules';
 import { PagerdutyProvider } from '@cdktf/provider-pagerduty';
 import { LocalProvider } from '@cdktf/provider-local';
@@ -36,7 +34,6 @@ class Stack extends TerraformStack {
 
     const region = new datasources.DataAwsRegion(this, 'region');
     const caller = new datasources.DataAwsCallerIdentity(this, 'caller');
-    const cache = Stack.createElasticache(this);
 
     const pocketApp = this.createPocketAlbApplication({
       pagerDuty: this.createPagerDuty(),
@@ -44,46 +41,9 @@ class Stack extends TerraformStack {
       snsTopic: this.getCodeDeploySnsTopic(),
       region,
       caller,
-      cache,
     });
 
     this.createApplicationCodePipeline(pocketApp);
-  }
-
-  /**
-   * Creates the elasticache and returns the node address list
-   * @param scope
-   * @private
-   */
-  private static createElasticache(scope: Construct): {
-    primaryEndpoint: string;
-    readerEndpoint: string;
-  } {
-    const pocketVPC = new PocketVPC(scope, 'pocket-vpc');
-
-    const elasticache = new ApplicationRedis(scope, 'redis', {
-      //Usually we would set the security group ids of the service that needs to hit this.
-      //However we don't have the necessary security group because it gets created in PocketALBApplication
-      //So instead we set it to null and allow anything within the vpc to access it.
-      //This is not ideal..
-      //Ideally we need to be able to add security groups to the ALB application.
-      allowedIngressSecurityGroupIds: undefined,
-      node: {
-        count: config.cacheNodes,
-        size: config.cacheSize,
-      },
-      subnetIds: pocketVPC.privateSubnetIds,
-      tags: config.tags,
-      vpcId: pocketVPC.vpc.id,
-      prefix: config.prefix,
-    });
-
-    return {
-      primaryEndpoint:
-        elasticache.elasticacheReplicationGroup.primaryEndpointAddress,
-      readerEndpoint:
-        elasticache.elasticacheReplicationGroup.readerEndpointAddress,
-    };
   }
 
   /**
@@ -162,7 +122,6 @@ class Stack extends TerraformStack {
     caller: datasources.DataAwsCallerIdentity;
     secretsManagerKmsAlias: kms.DataAwsKmsAlias;
     snsTopic: sns.DataAwsSnsTopic;
-    cache: { primaryEndpoint: string; readerEndpoint: string };
   }): PocketALBApplication {
     const {
       //  pagerDuty, // enable if necessary
@@ -170,7 +129,6 @@ class Stack extends TerraformStack {
       caller,
       secretsManagerKmsAlias,
       snsTopic,
-      cache,
     } = dependencies;
 
     return new PocketALBApplication(this, 'application', {
@@ -198,14 +156,6 @@ class Stack extends TerraformStack {
             {
               name: 'ENVIRONMENT',
               value: process.env.NODE_ENV, // this gives us a nice lowercase production and development
-            },
-            {
-              name: 'REDIS_PRIMARY_ENDPOINT',
-              value: cache.primaryEndpoint,
-            },
-            {
-              name: 'REDIS_READER_ENDPOINT',
-              value: cache.readerEndpoint,
             },
           ],
           secretEnvVars: [
@@ -304,7 +254,7 @@ class Stack extends TerraformStack {
 }
 
 const app = new App();
-const stack = new Stack(app, '');
+const stack = new Stack(app, 'firefox-api-proxy');
 const tfEnvVersion = fs.readFileSync('.terraform-version', 'utf8');
 stack.addOverride('terraform.required_version', tfEnvVersion);
 app.synth();
