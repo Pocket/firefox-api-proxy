f822f1fae22786f4797868560ce55863581ae4c6
Spencer Balogh <hyperparabolic@users.noreply.github.com>
2022-11-14 14:36:18 -0600

    fix(CI/CD): [BACK-1672] CI/CD fixes and tweaks for rest service (#6)
    * fix(CI/CD): [BACK-1672] CI/CD fixes and tweaks for rest service
    
    - Remove the apollo job and all references to it and schema.graphql
    - Change to cimg/node:18 images. circleci/* image tags are being deprecated,
      and this service is developed against node 18.
    - remove service setup from integration tests
    - file permissions, make .circleci/scripts/setup_hosts.sh executable
    - specify node 16 in buildspec.yml, 14 is default and is not compatible with npm latest.
    - backstage template fail, populate service name
    - uninstall and reinstall @pocket-tools/tsconfig to see if it fixes typescript linking
    - fix CODEOWNERS, it is supposed to be one line with both groups

--- .circleci/scripts/setup_hosts.sh
+++ .circleci/scripts/setup_hosts.sh
@@ -4,7 +4,7 @@ set -e
 echo "Adding service hosts records"
 
 # These should match the services you are creating in your docker-compose file
-declare -a arr=("snowplow")
+declare -a arr=()
 
 for i in "${arr[@]}"; do
     echo 127.0.0.1 "$i" | sudo tee -a /etc/hosts
