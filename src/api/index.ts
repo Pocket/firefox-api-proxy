f23338e395d05ad20ab9e9141d2598f9ee96570d
Spencer Balogh <sbalogh@mozilla.com>
2022-11-18 11:41:04 -0600

    feat(auth): [BACK-1661] Authentication middleware
    Added an authentication middleware, and registered it to the recent-saves route
    
    Also some documentation fixes around the production deployment url

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 const router = express.Router();
 
 import config from '../config';
@@ -9,7 +9,7 @@ import Desktop from './desktop';
 router
   // set Cache-control headers on all routes
   // move this further down the chain if we need control per-route.
-  .use((req, res, next) => {
+  .use((req: Request, res: Response, next: NextFunction) => {
     if (config.app.environment !== 'development') {
       // 30 minutes client side cache
       res.set('Cache-control', 'private, max-age=1800');
